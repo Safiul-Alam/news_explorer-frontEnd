a19a78f8580be45d332ceefbf2bb324e7c941312
Safiul alam, Mohammad <alam.buff@gmail.com>
2025-01-29 22:41:22 -0500

    .

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,39 +1,38 @@
 import { useLocation } from "react-router-dom";
-import Navigation from '../Navigation/Navigation';
-import './Header.css';
+import Navigation from "../Navigation/Navigation";
+import "./Header.css";
 
 function Header({
-    handleLoginClick,
-    isLoggedIn,
-    handleHomeClick,
-    handleSavedArticlesClick,
-    handleLogOut,
-    handleMobileMenuClick,
+  handleLoginClick,
+  isLoggedIn,
+  handleHomeClick,
+  handleSavedArticlesClick,
+  handleLogOut,
+  handleMobileMenuClick,
 }) {
+  const location = useLocation();
+  const isSavedNewsPage = location.pathname === "/saved-news";
 
-    const location = useLocation();
-    const savedNewsRoute = location.pathname === "/saved-news";
+  return (
+    <header className={`header ${isSavedNewsPage ? "header__saved-news" : ""}`}>
+      <button
+        className={`header__title ${isSavedNewsPage ? "header__title_saved-news" : ""}`}
+        onClick={handleHomeClick}
+        aria-label="Go to homepage"
+      >
+        NewsExplorer
+      </button>
 
-    return (
-        <header className={`header ${savedNewsRoute ? "header--saved-news" : ""}`}>
-            <button
-                className={`header__title ${savedNewsRoute ? "header__title__saved-news" : ""
-                    }`}
-                onClick={handleHomeClick}
-            >
-                NewsExplorer
-            </button>
-
-            <Navigation
-                handleLoginClick={handleLoginClick}
-                isLoggedIn={isLoggedIn}
-                handleHomeClick={handleHomeClick}
-                handleSavedArticlesClick={handleSavedArticlesClick}
-                handleLogOut={handleLogOut}
-                handleMobileMenuClick={handleMobileMenuClick}
-            />
-        </header>
-    );
+      <Navigation
+        handleLoginClick={handleLoginClick}
+        isLoggedIn={isLoggedIn}
+        handleHomeClick={handleHomeClick}
+        handleSavedArticlesClick={handleSavedArticlesClick}
+        handleLogOut={handleLogOut}
+        handleMobileMenuClick={handleMobileMenuClick}
+      />
+    </header>
+  );
 }
 
-export default Header
+export default Header;
